5e953bdf445cd716841c4369485e25f6916f16d3
wuxiang <1325668250@qq.com>
2021-12-04 13:34:00 +0800

    提交

---
+++ src/main/java/com/aisiono/store/entity/BaseEntity.java
@@ -0,0 +1,75 @@
+package com.aisiono.store.entity;
+
+import java.util.Date;
+import java.util.Objects;
+
+/**
+ * @author wuxiang
+ * @date 2021/12/4 1:16 下午
+ */
+public class BaseEntity {
+
+    private String createdUser;
+    private Date createdTime;
+    private String modifiedUser;
+    private Date modifiedTime;
+
+    public String getCreatedUser() {
+        return createdUser;
+    }
+
+    public void setCreatedUser(String createdUser) {
+        this.createdUser = createdUser;
+    }
+
+    public Date getCreatedTime() {
+        return createdTime;
+    }
+
+    public void setCreatedTime(Date createdTime) {
+        this.createdTime = createdTime;
+    }
+
+    public String getModifiedUser() {
+        return modifiedUser;
+    }
+
+    public void setModifiedUser(String modifiedUser) {
+        this.modifiedUser = modifiedUser;
+    }
+
+    public Date getModifiedTime() {
+        return modifiedTime;
+    }
+
+    public void setModifiedTime(Date modifiedTime) {
+        this.modifiedTime = modifiedTime;
+    }
+
+    @Override
+    public String toString() {
+        return "BaseEntity{" +
+                "createdUser='" + createdUser + '\'' +
+                ", createdTime=" + createdTime +
+                ", modifiedUser='" + modifiedUser + '\'' +
+                ", modifiedTime=" + modifiedTime +
+                '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        BaseEntity that = (BaseEntity) o;
+        return createdUser.equals(that.createdUser) && createdTime.equals(that.createdTime) && modifiedUser.equals(that.modifiedUser) && modifiedTime.equals(that.modifiedTime);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(createdUser, createdTime, modifiedUser, modifiedTime);
+    }
+}
